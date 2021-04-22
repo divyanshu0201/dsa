1ec1685667607a0defe9a590205adeb741323e64
divyanshu0201 <71318423+divyanshu0201@users.noreply.github.com>
2021-04-23 00:33:56 +0530

    Update 6_union_of_sorted_array.cpp

--- array/6_union_of_sorted_array.cpp
+++ array/6_union_of_sorted_array.cpp
@@ -1,117 +1,28 @@
-// { Driver Code Starts
-//Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
- // } Driver Code Ends
-//User function template in C++
-
-class Solution{
-    public:
-    //Function to return the count of number of elements in union of two arrays.
-    int doUnion(int a[], int n, int b[], int m)  {
-        int k=0;
-        int i=0,j=0;
-        int a1[n];
-        int b1[m];
-        int n1=0;
-        int m1=0;
-        for(i=0;i<n;i++)
+class Solution {
+  public:
+    vector<vector<int>> sortedMatrix(int N, vector<vector<int>> Mat) {
+        int sorted[N*N];
+        vector<vector<int>> Mat1(N);
+        int i,j,k=0;
+        for(i=0;i<N;i++)
         {
-            if(a[i]>0)
+            for(j=0;j<N;j++)
             {
-                for(j=i+1;j<n;j++)
-                {
-                    if(a[i]==a[j])
-                    {
-                        a[j]=-1;
-                    }
-                }
-            }    
-   
-        }
-        for(i=0;i<m;i++)
-        {
-            if(b[i]>0)
-            {
-                for(j=i+1;j<m;j++)
-                {
-                    if(b[i]==b[j])
-                    {
-                        b[j]=-1;
-                    }
-                }
-            }    
-   
-        }
-        for(i=0;i<n;i++)
-        {
-            if(a[i]>0)
-            {
-                a1[n1]=a[i];
-                n1++;
-            }
-        }
-        for(i=0;i<m;i++)
-        {
-            if(b[i]>0)
-            {
-                b1[m1]=b[i];
-                m1++;
-            }
-        }
-        i=0;
-        j=0;
-        while(i<n1&&j<m1)
-        {
-            if(a1[i]==b1[j])
-            {
+                sorted[k]=Mat[i][j];
                 k++;
             }
-            else
+        }
+        sort(sorted,sorted+(N*N));
+        k=0;
+        for(i=0;i<N;i++)
+        {
+            for(j=0;j<N;j++)
             {
-                k=k+2;
+                Mat1[i].push_back(sorted[k]);
+                k++;
             }
-            i++;
-            j++;
         }
-        while(i<n1)
-        {
-            k++;
-            i++;
-        }
-        while(j<m1)
-        {
-            k++;
-            j++;
-        }
-        return k;
+        return Mat1;
+        
     }
 };
-
-// { Driver Code Starts.
-
-int main() {
-	
-	int t;
-	cin >> t;
-	
-	while(t--){
-	    
-	    int n, m;
-	    cin >> n >> m;
-	    int a[n], b[m];
-	   
-	    for(int i = 0;i<n;i++)
-	       cin >> a[i];
-	       
-	    for(int i = 0;i<m;i++)
-	       cin >> b[i];
-	    Solution ob;
-	    cout << ob.doUnion(a, n, b, m) << endl;
-	    
-	}
-	
-	return 0;
-}  // } Driver Code Ends
