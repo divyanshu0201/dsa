972d126fd995c175b61ef9553a0b72e50b3226fe
divyanshu0201 <71318423+divyanshu0201@users.noreply.github.com>
2021-04-23 22:46:07 +0530

    Update 12_merging_2_sorted_array_without_extra_space.cpp

--- array/12_merging_2_sorted_array_without_extra_space.cpp
+++ array/12_merging_2_sorted_array_without_extra_space.cpp
@@ -1,52 +1,16 @@
-// { Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
- // } Driver Code Ends
-//User function template for C++
 class Solution{
 public:
 	void merge(int arr1[], int arr2[], int n, int m) {
 	    int i,j;
-	    for(i=m-1;i>=0;i--)
+	    for(i=0;i<n;i++)
 	    {
-	        int last=arr1[n-1];
-	        for(j=n-2;j>=0&&arr1[j]>arr2[i];j--)
-	        {
-	            arr1[j+1]=arr1[j];
-	        }
-	        if(j!=n-2||last>arr2[i])
-	        {
-	            arr1[j+1]=arr2[i];
-	            arr2[i]=last;
-	        }
+	            if(arr1[i]>arr2[0])
+	            {
+	                int temp=arr1[i];
+	                arr1[i]=arr2[j];
+	                arr2[j]=temp;
+	                sort(arr2,arr2+m);            
+	            }
 	    }
 	}
 };
-
-// { Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, m, i;
-        cin >> n >> m;
-        int arr1[n], arr2[m];
-        for (i = 0; i < n; i++) {
-            cin >> arr1[i];
-        }
-        for (i = 0; i < m; i++) {
-            cin >> arr2[i];
-        }
-        Solution ob;
-        ob.merge(arr1, arr2, n, m);
-        for (i = 0; i < n; i++) {
-            cout << arr1[i] << " ";
-        }
-        for (i = 0; i < m; i++) {
-            cout << arr2[i] << " ";
-        }
-        cout << "\n";
-    }
-    return 0;
-}  // } Driver Code Ends
