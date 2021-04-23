06fcbd14af5eaafb4194763f456a5880c57a19ee
divyanshu0201 <71318423+divyanshu0201@users.noreply.github.com>
2021-04-23 17:34:10 +0530

    Update 9_minimize_the_maximum_difference_between_array.cpp

--- array/9_minimize_the_maximum_difference_between_array.cpp
+++ array/9_minimize_the_maximum_difference_between_array.cpp
@@ -1,42 +1,19 @@
-int getMinDiff(int arr[], int n, int k)
+class Solution {
+  public:
+    int getMinDiff(int arr[], int n, int k)
 {
-    if (n == 1)
-       return 0;
- 
-    // Sort all elements
-    sort(arr, arr+n);
- 
-    // Initialize result
-    int ans = arr[n-1] - arr[0];
- 
-    // Handle corner elements
-    int small = arr[0] + k;
-    int big = arr[n-1] - k;
-    if (small > big)
-       swap(small, big);
- 
-    // Traverse middle elements
-    for (int i = 1; i < n-1; i ++)
-    {
-        int subtract = arr[i] - k;
-        int add = arr[i] + k;
- 
-        // If both subtraction and addition
-        // do not change diff
-        if (subtract >= small || add <= big)
-            continue;
- 
-        // Either subtraction causes a smaller
-        // number or addition causes a greater
-        // number. Update small or big using
-        // greedy approach (If big - subtract
-        // causes smaller diff, update small
-        // Else update big)
-        if (big - subtract <= add - small)
-            small = subtract;
-        else
-            big = add;
-    }
- 
-    return  min(ans, big - small);
+    int min1=0,max1=0,r=0;
+        sort(arr,arr+n);
+        r=arr[n-1]-arr[0];
+        for(int i=1;i<n;i++)
+        {
+            if(arr[i]>=k)
+            {
+            max1=max(arr[i-1]+k,arr[n-1]-k);
+            min1=min(arr[i]-k,arr[0]+k);
+            r=min(r,max1-min1);
+            }
+        }
+        return r;
 }
+};
