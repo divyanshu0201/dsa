77b7a0c6a74cc4470d7a633d6d96ba8afe1f9ddc
divyanshu0201 <71318423+divyanshu0201@users.noreply.github.com>
2021-04-23 21:39:14 +0530

    Update 10_minimum_number_of_jumps.cpp

--- array/10_minimum_number_of_jumps.cpp
+++ array/10_minimum_number_of_jumps.cpp
@@ -1,59 +1,39 @@
-// { Driver Code Starts
-#include<bits/stdc++.h>
-using namespace std;
-
-
- // } Driver Code Ends
-// Function to return minimum number of jumps to end of array
-
 class Solution{
   public:
     int minJumps(int arr[], int n){
-        int k=arr[0];
-        int d=0;
-        if(k==0)
+        int i;
+        int maxr=arr[0];
+        int step=arr[0];
+        int jump=1;
+        if(n==1)
         {
             return 0;
         }
-        int max=0;
-        int i=1,j=1,m=1;
-        while(j<n&&i<n)
+        else if(arr[0]==0)
         {
-            i=j;
-            while(i<k+m&&i<n)
-            {
-                if(arr[i]>max)
-                {
-                    max=arr[i];
-                    j=i;
-                }
-                i++;
-            }
-            m=j;
-            k=max;
-            d++;
+            return -1;
         }
-        return d;
+        else
+        {
+            for(i=1;i<n;i++)
+            {
+                if(i==n-1)
+                {
+                    return jump;
+                }
+                maxr=max(maxr,arr[i]+i);
+                step--;
+                if(step==0)
+                {
+                    jump++;
+                    if(i>=maxr)
+                    {
+                        return -1;
+                    }
+                    step=maxr-i;
+                }
+            }
+        }
+        return -1;
     }
 };
-
-
-// { Driver Code Starts.
-
-int main()
-{
-    int t;
-    cin>>t;
-    while(t--)
-    {
-        int n,i,j;
-        cin>>n;
-        int arr[n];
-        for(int i=0; i<n; i++)
-            cin>>arr[i];
-        Solution obj;
-        cout<<obj.minJumps(arr, n)<<endl;
-    }
-    return 0;
-}
-  // } Driver Code Ends
