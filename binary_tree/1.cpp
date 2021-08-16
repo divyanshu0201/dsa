7296c0ddd6c28adabfded305979d5fa603fd64da
divyanshu0201 <71318423+divyanshu0201@users.noreply.github.com>
2021-08-16 17:25:04 +0530

    Create 1.cpp

---
+++ binary_tree/1.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+    public:
+    //Function to return the level order traversal of a tree.
+    vector<int> levelOrder(Node* node)
+    {
+      vector<int> ans;
+      if(!node)
+        return ans;
+      queue<Node*> q;
+      q.push(node);
+      while(!q.empty()){
+          Node* t=q.front();
+          ans.push_back(t->data);
+          if(t->left)
+            q.push(t->left);
+          if(t->right)
+            q.push(t->right);
+          q.pop();    
+      }
+      return ans;
+    }
+};
